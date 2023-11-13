d5e69ba1468d717bb11affd335fa0aac8ba9885a
Telejean <raresvisanandrei@gmail.com>
2023-11-13 09:19:56 +0200

    multiple files setup

---
+++ POO_DBMS/getters.cpp
@@ -0,0 +1,8 @@
+#include "getters.h"
+#include <iostream>
+using namespace std;
+
+void testFunction()
+{
+	cout << "1-2 test 1-2" << endl;
+}
--- POO_DBMS/main.cpp
+++ POO_DBMS/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "getters.h"
 using namespace std;
+
+
 
 int main()
 {
+	testFunction();
 	cout << "hello world";
-	cout << "(. Y .)";
 	cout << " opel accident";
 }
